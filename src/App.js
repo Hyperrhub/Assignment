4e1bbd4de1f9041336c429ffa744a64cec011924
Hyperrhub <hiteshpatil2211@gmail.com>
2024-01-17 00:56:00 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import './App.css';
 import Assignment from './Assignment';
-// import AutoCompleteChips from './AutoCompleteChips';
-// import EmailChipInput from './EmailChipInput';
-// import ChipsArray from './ChipsArray';
-// import MultiSelect from './MultiSelect';
-// import CustomizedHook from './CustomizedHook';
-// import App from './MultiSelect';
+
 
 function App() {
-  const options = [{name:"hitesh", value:1 },{name:"bhushan",value:2},{name:"sandesh",value:3}];
   return (
     <div className="App">
-       {/* <MultiSelect options={options} /> */}
-       <Assignment options={options} ></Assignment>
-      
+      <Assignment></Assignment>
     </div>
   );
 }
--- src/Assignment.jsx
+++ src/Assignment.jsx
@@ -1,86 +1,126 @@
-import React, { useState } from 'react'
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./Assignment.css";
 
 const Itemsdata = [
   {
-    name :"Hitesh", key:1
+    name: "Hitesh",
+    key: 1,
+    img: "/dummy-image.jpg",
   },
-  {name:"kalpesh", key:2},
-  {name : "sandesh", key:3},
+  { name: "kalpesh", key: 2, img: "/dummy-image.jpg" },
+  { name: "sandesh", key: 3, img: "/dummy-image.jpg" },
   {
-    name :"bhaiya", key:4
+    name: "bhaiya",
+    key: 4,
+    img: "/dummy-image.jpg",
   },
   {
-    name :"bhai", key:5
+    name: "bhai",
+    key: 5,
+    img: "/dummy-image.jpg",
   },
   {
-    name :"bhya", key:6
-  }
-]
-const Assignment = ({options}) => {
-    const [inputValue, setInputValue]= useState("");
-    const [tags, setTags]= useState([]);
-    const addTags =(e)=>{
-        if(e.keyCode === 13 && inputValue ){
-            // alert(inputValue)
-            setTags([...tags, inputValue])
+    name: "brother",
+    key: 6,
+    img: "/dummy-image.jpg",
+  },
+];
+let t;
+const Assignment = ({ options }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [tags, setTags] = useState([]);
+  const [shouldShowList, setShouldList] = useState(false);
+  const inputRef = useRef();
 
-            setInputValue("")
-    //        
-        }
-    }
-    const removeTags =(val)=>{
-        let remain = tags.filter((t)=> t !==val)
-        console.log(val)
-       setTags(remain)
-    }
-    const onSearch =(searchItem)=>{
-    setInputValue(searchItem)
-      console.log("search",searchItem)
-    }
+  const removeTags = (val) => {
+    let remain = tags.filter((t) => t !== val);
+    setTags(remain);
+  };
+  const onSearch = (searchItem) => {
+    setTags((prevTags) => [...prevTags, searchItem]);
+  };
 
-    
- 
+  const filterOptions = Itemsdata.filter((item) => {
+    let shouldAdd = true;
+    for (let index = 0; index < tags.length; index++) {
+      const element = tags[index];
+      if (element.name === item.name) {
+        shouldAdd = false;
+        break;
+      }
+    }
+    return shouldAdd;
+  });
+
   return (
-    <div >
-      <div style={{
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignContent: "center",
-    marginTop:"50px"
-}}>
-        <div className='content' style={{ borderBottom:"3px solid blue",  width:"80%", alignItems:"center", textAlign:"center", display:"flex", alignContent:"center",justifyContent:"center"}}>
-           {tags.map((item, index)=>{
-           return <div key={index} style={{display:"flex",flexDirection:"row", marginRight:"5px"}}>
-            <div key={item} style={{display:"flex",flexDirection:"row",padding:"3px",backgroundColor:"lightgray", borderRadius:"10px", alignItems:"center", height:"30px" }}>
-                <p style={{backgroundColor:"lightgray"}}>
-                    {item}
-                </p>
-                <button key={index} onClick={()=>removeTags(item)}style={{ backgroundColor:"transparent", border:"none" , cursor:"pointer"}}>x</button>
-                </div>
-                </div>
-           })}
-            <input type='text' value={inputValue} placeholder='.. add new ' style={{ backgroundColor:"transparent", border:"none",outline:"none"}}
-            onChange={(e)=>{
-                setInputValue(e.target.value)
-            }} onKeyDown={addTags}></input>
-        </div>
-        <div style={{backgroundColor:"lightgray", width:"50%", alignItems:"centers", textAlign:"center",}}>
-            {inputValue &&
-            Itemsdata.filter(item=>{
-              const searchItem = inputValue.toLowerCase();
-              const name = item.name.toLowerCase()
-              return searchItem && name.startsWith(searchItem) && name !== searchItem
-            }).map((item, index)=>{
-              return  <div onClick={()=>onSearch(item.name)} style={{alignItems:"center",}} key={index}>
-                {item.name}</div>
-            })}
-        </div>
+    <div className="main" style={{ display: "flex" }}>
+      <div className="content">
+        {tags.map((item, index) => {
+          return (
+            <div className="input-main" key={index}>
+              <div className="input-submain" key={item}>
+                <img src={item.img} alt="" />
+                <p>{item.name}</p>
+                <button
+                  className="button-x"
+                  key={index}
+                  onClick={() => removeTags(item)}
+                >
+                  x
+                </button>
+              </div>
+            </div>
+          );
+        })}
+        <input
+          className="inputfield"
+          type="text"
+          value={inputValue}
+          placeholder=".. add new "
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          onFocus={() => {
+            t && clearTimeout(t);
+            setShouldList(true);
+          }}
+          onBlur={() => {
+            t = setTimeout(() => {
+              setShouldList(false);
+            }, 300);
+          }}
+          ref={inputRef}
+        ></input>
       </div>
-    
+      <div className="selectdiv">
+        <ul>
+          {shouldShowList &&
+            filterOptions
+              .filter((item) => {
+                const searchItem = inputValue.toLowerCase();
+                if (searchItem === "") return true;
+                const name = item.name.toLowerCase();
+                return searchItem && name.startsWith(searchItem);
+              })
+              .map((item, index) => {
+                return (
+                  <li
+                    className="selectdiv-sub"
+                    onClick={() => {
+                      onSearch(item);
+                      inputRef.current.focus();
+                    }}
+                    key={index}
+                  >
+                    <img src={"/dummy-image.jpg"} alt=""></img>
+                    {item.name}
+                  </li>
+                );
+              })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Assignment
+export default Assignment;
